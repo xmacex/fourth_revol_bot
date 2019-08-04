d0193d2fb066c373013900ae519a4e484ef8957d
Mace Ojala <mace.ojala@gmail.com>
2019-08-04 23:00:06 +0200

    Added minimal Twitter client thing which fetches the current timeline of it's own profile.

--- src/main/java/io/github/xmacex/fourth_revol_bot/App.java
+++ src/main/java/io/github/xmacex/fourth_revol_bot/App.java
@@ -1,13 +1,27 @@
 package io.github.xmacex.fourth_revol_bot;
 
+import java.util.List;
+import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.Status;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+	try {
+	    Twitter twitter = TwitterFactory.getSingleton();
+	    List<Status> statuses = twitter.getHomeTimeline();
+	    System.out.println("Showing home timeline.");
+	    for (Status status : statuses) {
+		System.out.println(status.getUser().getName() + ":" +
+				   status.getText());
+		System.out.println();
+	    }
+	} catch (Exception e) {
+	    System.err.println(e);
+	}
     }
 }
