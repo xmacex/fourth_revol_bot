6fd4ebe3896a1cc3d44afcc4b92c3332522fb317
Mace Ojala <maco@itu.dk>
2019-08-04 20:39:12 +0200

    Commiting a basic template ie archetype from Maven

---
+++ src/main/java/io/github/xmacex/fourth_revol_bot/App.java
@@ -0,0 +1,13 @@
+package io.github.xmacex.fourth_revol_bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
